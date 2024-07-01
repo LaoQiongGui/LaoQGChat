fc7d1a21a83f8bb84ddc64185bdbe32abfdefc2d
LaoQiongGui <2307712195@qq.com>
2024-07-02 04:42:47 +0800

    新增：配置CORS中间件

--- server.go
+++ server.go
@@ -4,7 +4,9 @@ import (
 	"LaoQGChat/controller"
 	"LaoQGChat/handler"
 	"LaoQGChat/service"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 var (
@@ -17,6 +19,17 @@ var (
 
 func main() {
 	server := gin.Default()
+
+	// 配置CORS中间件
+	config := cors.Config{
+		AllowAllOrigins:  true,                                     // 允许所有的域名
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"}, // 允许的HTTP方法
+		AllowHeaders:     []string{"Origin", "Content-Type"},       // 允许的请求头
+		ExposeHeaders:    []string{"Content-Length"},               // 暴露的头信息
+		AllowCredentials: true,                                     // 允许携带凭证
+		MaxAge:           12 * time.Hour,                           // 预检请求缓存时间
+	}
+	server.Use(cors.New(config))
 
 	server.POST("/Auth/Login", handler.HandlerBuilder(authController.Login))
 
