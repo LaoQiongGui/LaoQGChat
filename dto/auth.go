acfce85b5eb57269ca04a9af794ef2b5bf6e01e6
LaoQiongGui <2307712195@qq.com>
2024-07-21 23:44:32 +0800

    修改：Json标签修正

--- dto/auth.go
+++ dto/auth.go
@@ -5,6 +5,6 @@ import "github.com/google/uuid"
 type AuthDto struct {
 	Username   string    `json:"username"`
 	Password   string    `json:"password"`
-	LoginToken uuid.UUID `json:"login_token"`
+	LoginToken uuid.UUID `json:"loginToken"`
 	Permission string    `json:"permission"`
 }
